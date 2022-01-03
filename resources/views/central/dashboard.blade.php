406f6ff75360bd5e9be64a8f6d889aeba86836ca
Emmanuel Lampe <mail@emmanuel-lampe.de>
2022-01-03 01:51:35 +0100

    fixed header on dashboard

--- resources/views/central/dashboard.blade.php
+++ resources/views/central/dashboard.blade.php
@@ -1,4 +1,7 @@
 <x-central.app-layout>
+    <x-slot name="header">
+        {{ __('Dashboard') }}
+    </x-slot>
     <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6 shadow rounded-lg sm:w-1/2">
         Hey, welcome back {{ auth()->user()->name }}!
     </div>
--- resources/views/central/layouts/app.blade.php
+++ resources/views/central/layouts/app.blade.php
@@ -170,7 +170,7 @@
     <header class="relative py-10">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <h1 class="text-3xl font-bold text-gray-900">
-                Settings
+                {{ $header ?? 'undefined' }}
             </h1>
         </div>
     </header>
